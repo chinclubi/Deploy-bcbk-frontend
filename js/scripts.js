9006d2dfc117882a3736e14f6d871709fbafcf89
ChinCluBi <chincub@gmail.com>
2015-11-14 03:56:56 +0700

    resend email

--- js/scripts.js
+++ js/scripts.js
@@ -9222,6 +9222,7 @@ return jQuery;
       'controller.register',
       'controller.whoscoming',
       'controller.edituser',
+      'controller.resend',
       'directive.validation',
       'directive.interestValidation'
     // 'directive.navbar'
@@ -28070,6 +28071,10 @@ angular.module('ui.select').run(['$templateCache', function ($templateCache) {$t
           angular.forEach($scope.register.$error.email, function (value, key) {
             value.$setDirty(true)
           })
+        } else if ($scope.register.$error.pattern) {
+          angular.forEach($scope.register.$error.patter, function (value, key) {
+            value.$setDirty(true)
+          })
         } else if ($scope.register.$error.emailvalid) {
           angular.forEach($scope.register.$error.emailvalid, function (value, key) {
             value.$setDirty(true)
@@ -28156,6 +28161,90 @@ angular.module('ui.select').run(['$templateCache', function ($templateCache) {$t
 
 ;(function () {
   angular
+    .module('controller.resend', ['vcRecaptcha'])
+    .controller('ResendMailController', ResendMailController)
+
+  ResendMailController.$inject = ['$scope', '$http', '$q', 'vcRecaptchaService']
+  function ResendMailController ($scope, $http, $q, vcRecaptchaService) {
+    var self = this
+    self.model = {
+      key: '6Le75hATAAAAADVNCfVui0mQJ1OYV2XVAfsHXCoQ'
+    }
+    self.submit = {
+      isSubmit: false,
+      status: null
+    }
+
+    self.response = null
+    self.widgetId = null
+    self.resend = resend
+    self.setResponse = response
+    self.setWidgetId = setWidgetId
+    self.cbExpiration = cbExpiration
+
+    var form = {
+      resendForm: $('#resend-form'),
+      load: $('#loading'),
+      resendBtn: $('#resend-btn'),
+      successForm: $('#success-resend')
+    }
+
+    form.load.hide()
+    form.successForm.hide()
+
+    function resend () {
+      self.submit.isSubmit = true
+      if ($scope.resend.$invalid) {
+        if ($scope.resend.$error.required) {
+          angular.forEach($scope.resend.$error.required, function (value, key) {
+            value.$setDirty(true)
+          })
+        } else if ($scope.resend.$error.email) {
+          angular.forEach($scope.resend.$error.email, function (value, key) {
+            value.$setDirty(true)
+          })
+        } else if ($scope.resend.$error.pattern) {
+          angular.forEach($scope.resend.$error.patter, function (value, key) {
+            value.$setDirty(true)
+          })
+        }
+      } else {
+        $http({
+          method: 'POST',
+          url: 'http://api.barcampbangkhen.org/resend',
+          data: {
+            email: self.email,
+            response: self.response
+          }
+        }).success(function (response, status) {
+          self.submit.status = status
+        }).error(function (response, status) {
+          self.submit.status = status
+          if (status === 404) {
+            vcRecaptchaService.reload(self.widgetId)
+          }
+        })
+      }
+    }
+
+    function response (response) {
+      self.response = response
+    }
+
+    function setWidgetId (widgetId) {
+      self.widgetId = widgetId
+    }
+    function cbExpiration () {
+      self.response = null
+    }
+  }
+})()
+
+/* global angular */
+/* global $ */
+
+;(function () {
+  angular
     .module('controller.whoscoming', ['ui.select'])
     .config(['uiSelectConfig', function (uiSelectConfig) {
       uiSelectConfig.theme = 'bootstrap'
@@ -28211,6 +28300,52 @@ angular.module('ui.select').run(['$templateCache', function ($templateCache) {$t
       })
       self.people = data
     })
+  }
+})()
+
+/* global angular */
+
+;(function () {
+  angular
+    .module('services.route', ['ui.router'])
+    .config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider']
+
+  function config ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('', '/home')
+    $urlRouterProvider.otherwise('/home')
+    $stateProvider
+      .state('home', {
+        url: '/home?section',
+        templateUrl: 'templates/home.tmpl',
+        controller: 'HomePageController',
+        controllerAs: 'homepageCtrl'
+      })
+      .state('whoscoming', {
+        url: '/whoscoming',
+        templateUrl: 'templates/whoscoming.html',
+        controller: 'WhoscomingController',
+        controllerAs: 'WhoscomingCtrl'
+      })
+      .state('edituser', {
+        url: '/editprofile?email&c',
+        templateUrl: 'templates/edituser.html',
+        controller: 'EditUserController',
+        controllerAs: 'editCtrl'
+      })
+      .state('resendmail', {
+        url: '/resend',
+        templateUrl: 'templates/resendmail.html',
+        controller: 'ResendMailController',
+        controllerAs: 'resendCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController',
+        controllerAs: 'AboutCtrl'
+      })
   }
 })()
 
@@ -28289,45 +28424,5 @@ angular.module('ui.select').run(['$templateCache', function ($templateCache) {$t
         }, 500)
       }
     }
-  }
-})()
-
-/* global angular */
-
-;(function () {
-    angular
-        .module('services.route', ['ui.router'])
-        .config(config)
-
-    config.$inject = ['$stateProvider', '$urlRouterProvider']
-
-  function config ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('', '/home')
-    $urlRouterProvider.otherwise('/home')
-    $stateProvider
-      .state('home', {
-        url: '/home?section',
-        templateUrl: 'templates/home.tmpl',
-        controller: 'HomePageController',
-        controllerAs: 'homepageCtrl'
-      })
-      .state('whoscoming', {
-        url: '/whoscoming',
-        templateUrl: 'templates/whoscoming.html',
-        controller: 'WhoscomingController',
-        controllerAs: 'WhoscomingCtrl'
-      })
-      .state('edituser', {
-        url: '/editprofile?email&c',
-        templateUrl: 'templates/edituser.html',
-        controller: 'EditUserController',
-        controllerAs: 'editCtrl'
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'templates/about.html',
-        controller: 'AboutController',
-        controllerAs: 'AboutCtrl'
-      })
   }
 })()
